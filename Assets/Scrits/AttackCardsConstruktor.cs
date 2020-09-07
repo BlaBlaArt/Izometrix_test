fa5199172dcc96368e4e9c119c29df9fdfcee12d
BlaBlaArt <ivanovartemw@gmail.com>
2020-09-07 14:15:17 +0300

    update

---
+++ Assets/Scrits/AttackCardsConstruktor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackCardsConstruktor : MonoBehaviour
+{
+    public int AttackDamage;
+
+    public int TimeCoolDown;
+
+    public int StaminaNeeds;
+
+
+}
