473e483d82c3a46e09eb0d3ac9a0fc785d9b307d
BlaBlaArt <ivanovartemw@gmail.com>
2020-09-07 12:41:41 +0300

    UI attack Scene

---
+++ Assets/Scrits/ButtonSettingsBattleSceneScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSettingsBattleSceneScript : MonoBehaviour
+{
+    public Canvas MainScene;
+    public Canvas AttackScene;
+
+    public void AttackEnabled()
+    {
+        MainScene.enabled = false;
+        AttackScene.enabled = true;
+    }
+
+    public void AttackDisabled()
+    {
+        MainScene.enabled = true;
+        AttackScene.enabled = false;
+    }
+}
